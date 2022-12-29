1fc374565b84764a10ea0bc2a716c78511e5c30b
Szymon Wnuk <101000022+HelloSzymon@users.noreply.github.com>
2022-12-29 11:10:55 +0100

    Initial Commit

---
+++ iPhone NumberField/iPhone_NumberFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhone_NumberFieldApp.swift
+//  iPhone NumberField
+//
+//  Created by Szymon Wnuk on 29/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iPhone_NumberFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
